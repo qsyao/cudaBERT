53eab501d9aa2c3f0084e20f5382b65aeacce423
Qingsong Yao <v-qiyao@microsoft.com>
2019-05-13 02:20:47 -0700

    speed up softmax backward

--- ops/softmax.cu
+++ ops/softmax.cu
@@ -44,7 +44,6 @@ void cuWelfordMax(
 }
 
 template<typename T> __device__ __forceinline__
-
 void cuWelfordSum(
         const T *vals,
         const int n1,
@@ -88,8 +87,7 @@ __global__
 void cuApplySoftmax(
         T *tensor,
         const int n1,
-        const int n2
-) {
+        const int n2){
     // Assumptions:
     // 1) blockDim.x == warpSize
     // 2) Tensors are contiguous
@@ -136,29 +134,63 @@ void op_SoftMax::forward<float>(
         size_t n2
 );
 
+template<typename T> __device__ __forceinline__
+void cuWelfordGradSum(
+        const T *vals,
+        const T *grad,
+        const int n1,
+        const int n2,
+        const int i1,
+        T &sum) {
+    sum = T(0);
+    if (i1 < n1) {
+        // one warp normalizes one n1 index,
+        // synchronization is implicit
+        // initialize with standard Welford algorithm
+        const int numx = blockDim.x * blockDim.y;
+        const int thrx = threadIdx.x + threadIdx.y * blockDim.x;
+        const T *lvals = vals + i1 * n2;
+        const T *lgrad = grad + i1 * n2;
+        int l = 4 * thrx;
+        for (; l + 3 < n2; l += 4 * numx) {
+            for (int k = 0; k < 4; ++k) {
+                T curr = static_cast<T>(lvals[l + k]) * static_cast<T>(lgrad[l + k]);
+                sum += curr;
+            }
+        }
+        for (; l < n2; ++l) {
+            T curr =  static_cast<T>(lvals[l]) * static_cast<T>(lgrad[l]);
+            sum += curr;
+        }
+        // intra-warp reductions
+        for (int l = 0; l <= 4; ++l) {
+            int srcLaneB = (threadIdx.x + (1 << l)) & 31;
+            T sumB = WARP_SHFL(sum, srcLaneB);
+            sum += sumB;
+        }
+        // threadIdx.x == 0 has correct values for each warp
+        // inter-warp reductions{
+        sum = WARP_SHFL(sum, 0);
+    }
+}
+
 // TODO: speed up softmax backward
 template<typename T>
 __global__
 void cuApplySoftmaxBackward(T *dout, T *forward_output, T *grad_input, size_t n1, size_t n2) {
-    for (int i1 = blockIdx.x; i1 < n1; i1 += gridDim.x) {
-        for (int i2 = threadIdx.x; i2 < n2; i2 += blockDim.x) {
-            grad_input[i2 + i1 * n2] = 0;
-            for (int i3 = 0; i3 < n2; i3++)
-                grad_input[i2 + i1 * n2] -=
-                        forward_output[i2 + i1 * n2] * forward_output[i3 + i1 * n2] * dout[i3 + i1 * n2];
-            grad_input[i2 + i1 * n2] +=
-                    forward_output[i2 + i1 * n2] * forward_output[i2 + i1 * n2] * dout[i2 + i1 * n2];
-            grad_input[i2 + i1 * n2] +=
-                    forward_output[i2 + i1 * n2] * (1.0 - forward_output[i2 + i1 * n2]) * dout[i2 + i1 * n2];
-        }
-    }
+    int i1 = blockIdx.x;
+    int i2 = threadIdx.x;
+    T tmp = forward_output[i2 + i1 * n2];
+    T sum;
+    cuWelfordGradSum(forward_output, dout, n1, n2, i1, sum);
+    grad_input[i2 + i1 * n2] = tmp * dout[i2 + i1 * n2] - sum * tmp;
     __syncthreads();
 }
 
 template<typename T>
 void op_SoftMax::backward(T *dout, size_t n1, size_t n2) {
-    const dim3 threads(min((long) 1024, n2), 1, 1);
-    const dim3 blocks(min((long) 65535, n1), 1, 1);
+    const dim3 threads(n2, 1, 1);
+    const dim3 blocks(n1, 1, 1);
     grad_input = handle->global_malloc_manage_float.get_new_head_point(n1 * n2);
     cuApplySoftmaxBackward << < blocks, threads, 0, handle->cal_stream >> > (dout, stored_output, grad_input, n1, n2);
 }
--- test/test.cpp
+++ test/test.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[]){
    if (argc == 1)
-       test(4, 11, 1, false);
+       test_train(4, 11, 1, false);
    else{
-      test(std::stoi(argv[1]), std::stoi(argv[2]), std::stoi(argv[3]), false, std::stoi(argv[4]));
+      test_train(std::stoi(argv[1]), std::stoi(argv[2]), std::stoi(argv[3]), false, std::stoi(argv[4]));
    }
    return 0;
 }
